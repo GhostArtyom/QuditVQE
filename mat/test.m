277234fbf7354de143459717e59d90c493888a7c
GhostArtyom <ghostartyomns@gmail.com>
2023-09-05 11:50:38 +0800

    Add mat

---
+++ mat/test.m
@@ -0,0 +1,10 @@
+clear
+
+rdm = load('322_d2_num1_model957_RDM.mat')
+% g = load('322_d2_num1_model957_body1_G_gates.mat')
+g = load('322_d2_num1_model957_site3_G_gates.mat');
+
+% g.Q_gates_40{:}
+rdm.RDM_1
+rdm.RDM_2
+rdm.RDM_3

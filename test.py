eaa9e7de94df9fc38f4244ee4a508a5c6055fed5
GhostArtyom <ghostartyomns@gmail.com>
2023-09-11 13:04:05 +0800

    Update one_qubit_decompose

--- test.py
+++ test.py
@@ -1,19 +1,55 @@
+import re
+import os
+import sys
+import h5py
+import time
 import numpy as np
+import scipy as sp
 from utils import *
-from numpy.linalg import norm
+from numpy.linalg import det, norm
+from scipy.sparse import csr_matrix
+from mindquantum.framework import *
+from mindquantum.core.gates import *
+from scipy.stats import unitary_group
+from mindquantum.core.circuit import *
+from IPython.display import display_svg
+from mindquantum.core.operators import *
+from mindquantum.algorithm.nisq import *
+from mindquantum.simulator import Simulator
+from mindquantum.algorithm.compiler import *
+from mindquantum.algorithm.compiler.decompose.utils import *
 
 np.set_printoptions(linewidth=200)
 
-d = 10
-qudit = np.random.rand(d) + 1j * np.random.rand(d)
-qudit /= norm(qudit)
-qudit = np.outer(qudit.conj(), qudit)
-print(qudit.shape, qudit.trace())
-qubits = SU2_Encoding(qudit)
-print(qubits.shape, qubits.trace())
+# d = 10
+# qudit = np.random.rand(d) + 1j * np.random.rand(d)
+# qudit /= norm(qudit)
+# qudit = np.outer(qudit.conj(), qudit)
+# print(qudit.shape, qudit.trace())
+# qubits = su2_encoding(qudit)
+# print(qubits.shape, qubits.trace())
 
-qudit = np.random.rand(d) + 1j * np.random.rand(d)
-qudit /= norm(qudit)
-qubits = SU2_Encoding(qudit)
-print(qudit.shape)
-print(qubits.shape)
+# qudit = np.random.rand(d) + 1j * np.random.rand(d)
+# qudit /= norm(qudit)
+# qubits = su2_encoding(qudit)
+# print(qudit.shape)
+# print(qubits.shape)
+
+d = 2
+gate = unitary_group.rvs(d, random_state=42)
+print(gate)
+gate_u = UnivMathGate('gate', gate).on(0)
+gate_d, pr = one_qubit_decompose(gate_u, 'zyz')
+print(gate_d)
+print(gate_d.matrix(pr))
+print(gate_d.apply_value(pr))
+print(norm(gate - gate_d.matrix(pr)))
+
+gate_d = euler_decompose(gate_u, 'zyz')
+print(gate_d)
+print(gate_d.matrix())
+print(norm(gate - gate_d.matrix()))
+
+print(pr)
+print(params_zyz(gate))
+print(params_u3(gate))
--- utils.py
+++ utils.py
@@ -1,10 +1,77 @@
+import os
+import sys
+import pickle
 import numpy as np
-from numpy.linalg import norm
+from typing import List
+from numpy.linalg import det, norm
 from scipy.linalg import sqrtm
 from scipy.sparse import csr_matrix
+from mindquantum.core.circuit import Circuit
+from mindquantum.core.gates import RX, RY, RZ, U3, GlobalPhase, UnivMathGate
+
+optional_basis = ['zyz', 'u3']
 
 
-def fidelity(rho, sigma):
+def one_qubit_decompose(gate: UnivMathGate, basis: str = 'zyz', with_phase: bool = True) -> Circuit:
+    str_phase = gate.name + '_phase'
+    str_theta = gate.name + '_theta'
+    str_phi = gate.name + '_phi'
+    str_lam = gate.name + '_lam'
+    obj = gate.obj_qubits
+    mat = gate.matrix()
+    d = mat.shape[0]
+    circ = Circuit()
+    if not np.allclose(np.eye(2), mat @ mat.conj().T):
+        raise ValueError('The gate is not unitary')
+    phase = -np.angle(det(mat)) / 2
+    matU = mat * np.exp(1j * phase)
+    cos = np.sqrt(np.real(matU[0, 0] * matU[1, 1]))
+    sin = np.sqrt(-np.real(matU[1, 0] * matU[0, 1]))
+    theta = 2 * np.arccos(cos)
+    phi = np.angle(matU[1, 1]) + np.angle(matU[1, 0])
+    lam = np.angle(matU[1, 1]) - np.angle(matU[1, 0])
+    if basis == 'zyz':
+        circ += RZ(str_lam).on(obj)
+        circ += RY(str_theta).on(obj)
+        circ += RZ(str_phi).on(obj)
+        if with_phase:
+            circ += GlobalPhase(str_phase).on(obj)
+    elif basis == 'u3':
+        circ += U3(str_theta, str_phi, str_lam).on(obj)
+        phase += (phi + lam) / 2
+        if with_phase:
+            circ += GlobalPhase(str_phase).on(obj)
+    else:
+        raise ValueError(f'{basis} is not a supported decomposition method of {optional_basis}')
+    pr = {str_phase: phase, str_phi: phi, str_theta: theta, str_lam: lam}
+    return circ, pr
+
+
+def partial_trace(rho: np.array, index: List[int]) -> np.array:
+    nq = int(rho.shape[0] / 2)
+    d = int(np.log2(nq))
+    pt = np.zeros([nq, nq], dtype=complex)
+    for i in range(nq):
+        i_ = bin(i)[2::].zfill(d)
+        i0 = int(i_[:index] + '0' + i_[index:], 2)
+        i1 = int(i_[:index] + '1' + i_[index:], 2)
+        for j in range(nq):
+            j_ = bin(j)[2::].zfill(d)
+            j0 = int(j_[:index] + '0' + j_[index:], 2)
+            j1 = int(j_[:index] + '1' + j_[index:], 2)
+            pt[i, j] = rho[i0, j0] + rho[i1, j1]
+    return pt
+
+
+def reduced_density_matrix(rho: np.array, position: List[int]) -> np.array:
+    nq = int(np.log2(rho.shape[0]))
+    p = [x for x in range(nq) if x not in position]
+    for i in p[::-1]:
+        rho = partial_trace(rho, i)
+    return rho
+
+
+def fidelity(rho: np.array, sigma: np.array) -> float:
     if rho.ndim == 2 and (rho.shape[0] == 1 or rho.shape[1] == 1):
         rho = rho.flatten()
     if sigma.ndim == 2 and (sigma.shape[0] == 1 or sigma.shape[1] == 1):
@@ -22,47 +89,29 @@ def fidelity(rho, sigma):
     return f
 
 
-def partial_trace(rho, index):
-    nq = int(rho.shape[0] / 2)
-    d = int(np.log2(nq))
-    pt = np.zeros([nq, nq], dtype=complex)
-    for i in range(nq):
-        i_ = bin(i)[2::].zfill(d)
-        i0 = int(i_[:index] + '0' + i_[index:], 2)
-        i1 = int(i_[:index] + '1' + i_[index:], 2)
-        for j in range(nq):
-            j_ = bin(j)[2::].zfill(d)
-            j0 = int(j_[:index] + '0' + j_[index:], 2)
-            j1 = int(j_[:index] + '1' + j_[index:], 2)
-            pt[i, j] = rho[i0, j0] + rho[i1, j1]
-    return pt
-
-
-def reduced_density_matrix(rho, position):
-    nq = int(np.log2(rho.shape[0]))
-    p = [x for x in range(nq) if x not in position]
-    for i in p[::-1]:
-        rho = partial_trace(rho, i)
-    return rho
-
-
-def SU2_Encoding(qudit):
+def su2_encoding(qudit: np.array) -> np.array:
     d = np.shape(qudit)
-    if np.iscomplex(qudit).any():
-        dtype = 'complex'
-    else:
-        dtype = 'float64'
     if len(d) == 2 and d[0] == d[1]:
         d = d[0]
         nq = d - 1
-        qubits = csr_matrix(np.zeros([2**nq, 2**nq], dtype))
-        nq_bin = {}
-        for i in range(2**nq):
-            num1 = bin(i).count('1')
-            if num1 in nq_bin:
-                nq_bin[num1].append(i)
-            else:
-                nq_bin[num1] = [i]
+        qubits = csr_matrix(np.zeros([2**nq, 2**nq], dtype=np.complex128))
+        if d < 15:
+            nq_bin = {}
+            for i in range(2**nq):
+                num1 = bin(i).count('1')
+                if num1 in nq_bin:
+                    nq_bin[num1].append(i)
+                else:
+                    nq_bin[num1] = [i]
+        elif d >= 15 and d <= 25:
+            name = 'nq_bin_d=%d.pkl' % d
+            path = os.path.join(sys.path[0], 'nq_bin', name)
+            f_read = open(path, 'rb')
+            nq_bin = pickle.load(f_read)
+            f_read.close()
+        else:
+            nq_bin = [[0]] * d
+            raise ValueError('d is over 25!')
         for i in range(d):
             qubits_i = nq_bin[i]
             num_i = len(qubits_i)
@@ -75,7 +124,7 @@ def SU2_Encoding(qudit):
                 data = np.ones(num_i * num_j) * qudit[i, j] / div
                 qubits += csr_matrix((data, (ii, jj)), shape=(2**nq, 2**nq))
         qubits = qubits.toarray()
-    elif len(d) == 1 or (len(d) == 2 and (d[0] == 1 or d[1] == 1)):
+    elif len(d) == 1 or (len(d) == 2 and d[0] == 1 or d[1] == 1):
         if len(d) == 2 and d[0] == 1:
             qudit = qudit.flatten()
             d = d[1]
@@ -85,14 +134,24 @@ def SU2_Encoding(qudit):
         else:
             d = d[0]
         nq = d - 1
-        qubits = csr_matrix(np.zeros(2**nq, dtype))
-        nq_bin = {}
-        for i in range(2**nq):
-            num1 = bin(i).count('1')
-            if num1 in nq_bin:
-                nq_bin[num1].append(i)
-            else:
-                nq_bin[num1] = [i]
+        qubits = csr_matrix(np.zeros(2**nq, dtype=np.complex128))
+        if d < 15:
+            nq_bin = {}
+            for i in range(2**nq):
+                num1 = bin(i).count('1')
+                if num1 in nq_bin:
+                    nq_bin[num1].append(i)
+                else:
+                    nq_bin[num1] = [i]
+        elif d >= 15 and d <= 25:
+            name = 'nq_bin_d=%d.pkl' % d
+            path = os.path.join(sys.path[0], 'nq_bin', name)
+            f_read = open(path, 'rb')
+            nq_bin = pickle.load(f_read)
+            f_read.close()
+        else:
+            nq_bin = [[0]] * d
+            raise ValueError('d is over 25!')
         for i in range(d):
             qubits_i = nq_bin[i]
             num_i = len(qubits_i)

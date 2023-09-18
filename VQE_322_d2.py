43bf0686273b5a11bc8d22b5385812a2d3f042fc
GhostArtyom <ghostartyomns@gmail.com>
2023-09-18 18:15:35 +0800

    Finally right for loss=fidelity by 1-f, -g

--- VQE_322_d2.py
+++ VQE_322_d2.py
@@ -3,7 +3,6 @@ import h5py
 import time
 import numpy as np
 from utils import *
-import mindspore as ms
 from numpy.linalg import norm
 from scipy.sparse import csr_matrix
 from scipy.optimize import minimize
@@ -11,26 +10,27 @@ from mindquantum.simulator import Simulator
 from mindquantum.core.circuit import Circuit
 from mindquantum.core.gates import UnivMathGate
 from mindquantum.core.operators import Hamiltonian
+from mindquantum.simulator import get_supported_simulator
 
 start = time.perf_counter()
 np.set_printoptions(linewidth=200)
-device_target = ms.get_context("device_target")
 
 
-def fun(p0, sim_grad, iter_list=None):
+def fun(p0, sim_grad, args=None):
     f, g = sim_grad(p0)
-    f = np.real(f)[0, 0]
-    g = np.real(g)[0, 0]
-    if iter_list is not None:
-        iter_list.append(f)
-        i = len(iter_list)
-        if i % 1 == 0:
+    f = 1 - np.real(f)[0][0]
+    g = -np.real(g)[0][0]
+    if args is not None:
+        args.append(f)
+        i = len(args)
+        if i % 10 == 0:
             print('Optimal Gap: %.12f, %d' % (f, i))
     return f, g
 
 
 g = h5py.File('./mat/322_d2_num1_model957_site3_gates_L39_N9_zhu(2).mat', 'r')
 # position = g['position'][:] - 1  # subtract index of matlab to python
+position = [3, 4, 5]
 l = list(g.keys())  # list of HDF5 file keys
 g_name = [x for x in l if 'gates' in x]  # list of Q_gates_?
 key = lambda x: [int(s) if s.isdigit() else s for s in re.split('(\d+)', x)]
@@ -68,40 +68,51 @@ params_name = ansatz.ansatz_params_name
 params_size = len(params_name)
 print('Number of params: %d' % params_size)
 
-ham = np.kron(np.kron(rdm[3], rdm[3]), rdm[3])
+sim = Simulator('mqvector', ansatz.n_qubits)
+sim.apply_circuit(ansatz.apply_value(gate_pr))
+psi = sim.get_qs()
+rho = np.outer(psi, psi.conj())
+ham = rho
+# ham = np.kron(np.kron(rdm[3], rdm[3]), rdm[3])
 print('Hamiltonian Dimension:', ham.shape)
 Ham = Hamiltonian(csr_matrix(ham))
 
-if device_target == 'CPU':
-    sim = Simulator('mqvector', ansatz.n_qubits)
-elif device_target == 'GPU':
+sim_list = set([i[0] for i in get_supported_simulator()])
+print(sim_list)
+if 'mqvector_gpu' in sim_list:
     sim = Simulator('mqvector_gpu', ansatz.n_qubits)
 else:
-    raise ValueError(f'{device_target} is not applicable')
+    sim = Simulator('mqvector', ansatz.n_qubits)
 sim_grad = sim.get_expectation_with_grad(Ham, ansatz)
-p0 = np.zeros(params_size)
-f, g = sim_grad(p0)
 
+gate_pr = {i: gate_pr[i] for i in params_name}
+# p0 = np.array(list(gate_pr.values())) + np.random.uniform(-1, 1, params_size)
+p0 = np.random.uniform(-1, 1, params_size)
 fun(p0, sim_grad)
-iter_list = []
-res = minimize(fun, p0, args=(sim_grad, iter_list), method='bfgs', jac=True, tol=1e-6)
+res = minimize(fun, p0, args=(sim_grad, []), method='bfgs', jac=True, tol=1e-6)
 
 print(res.message)
 print('Optimal Value: %.12f' % res.fun)
 
 sim.reset()
+sim.apply_circuit(ansatz.apply_value(gate_pr))
+psi = sim.get_qs()
+rho = np.outer(psi, psi.conj())
+rho_rdm = reduced_density_matrix(rho, position)
+
+sim.reset()
 res_pr = dict(zip(params_name, res.x))
 sim.apply_circuit(ansatz.apply_value(res_pr))
 psi_res = sim.get_qs()
-sim.reset()
-sim.apply_circuit(ansatz.apply_value(gate_pr))
-psi = sim.get_qs()
-rho = np.outer(psi_res, psi_res.conj())
-rho = reduced_density_matrix(rho, [3, 4, 5])
+rho_res = np.outer(psi_res, psi_res.conj())
+rho_res_rdm = reduced_density_matrix(rho_res, position)
+
 print('psi norm: %.20f' % norm(psi - psi_res, 2))
 print('psi fidelity: %.20f' % fidelity(psi, psi_res))
-print('rdm[3] norm: %.20f' % norm(rdm[3] - rho, 2))
-print('rdm[3] fidelity: %.20f' % fidelity(rdm[3], rho))
+print('rho norm: %.20f' % norm(rdm[3] - rho_rdm, 2))
+print('rho fidelity: %.20f' % fidelity(rdm[3], rho_rdm))
+print('res norm: %.20f' % norm(rdm[3] - rho_res_rdm, 2))
+print('res fidelity: %.20f' % fidelity(rdm[3], rho_res_rdm))
 
 end = time.perf_counter()
 print('Runtime: %f' % (end - start))

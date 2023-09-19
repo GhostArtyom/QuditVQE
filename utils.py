18615de96b586821077f1aae28d3e98ccb09b3c2
GhostArtyom <ghostartyomns@gmail.com>
2023-09-19 11:48:04 +0800

    Fix: no clip for fidelity, add sqrt for f or f**2

--- utils.py
+++ utils.py
@@ -201,7 +201,7 @@ def reduced_density_matrix(rho: np.ndarray, position: List[int]) -> np.ndarray:
     return rho
 
 
-def fidelity(rho: np.ndarray, sigma: np.ndarray) -> float:
+def fidelity(rho: np.ndarray, sigma: np.ndarray, sqrt: bool = True) -> float:
     if rho.ndim == 2 and (rho.shape[0] == 1 or rho.shape[1] == 1):
         rho = rho.flatten()
     if sigma.ndim == 2 and (sigma.shape[0] == 1 or sigma.shape[1] == 1):
@@ -209,14 +209,17 @@ def fidelity(rho: np.ndarray, sigma: np.ndarray) -> float:
     if rho.ndim == 1 and sigma.ndim == 1:
         f = np.abs(rho.conj() @ sigma)
     elif rho.ndim == 1 and sigma.ndim == 2:
-        f = np.real(np.sqrt(rho.conj().T @ sigma @ rho))
+        f = np.sqrt(np.real(rho.conj().T @ sigma @ rho))
     elif rho.ndim == 2 and sigma.ndim == 1:
-        f = np.real(np.sqrt(sigma.conj().T @ rho @ sigma))
+        f = np.sqrt(np.real(sigma.conj().T @ rho @ sigma))
     elif rho.ndim == 2 and sigma.ndim == 2:
         f = np.real(np.trace(sqrtm(sqrtm(rho) @ sigma @ sqrtm(rho))))
     else:
         raise ValueError('Wrong Input!')
-    return np.clip(f, -1, 1)
+    if sqrt:
+        return f
+    else:
+        return f**2
 
 
 def su2_encoding(qudit: np.ndarray) -> np.ndarray:

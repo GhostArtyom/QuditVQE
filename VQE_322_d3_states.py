c9398a7f59fbfb9689ae7504f917b7d37e02e0b2
GhostArtyom <ghostartyomns@gmail.com>
2024-01-16 14:50:51 +0800

    Feature: VQE_322_d3_states for multi states Upload: 322 target_state_vector mat

--- VQE_322_d3_states.py
+++ VQE_322_d3_states.py
@@ -28,7 +28,7 @@ def fun(p0, sim_grad, loss_list=None):
         if i % 10 == 0:
             global start, num, layers
             t = time.perf_counter() - start
-            print(f'num{num}, {model}, Layers: {layers}, ', end='')
+            print(f'num{num}, {model}, vec{vec}, Layers: {layers}, ', end='')
             print(f'Loss: {f:.15f}, Fidelity: {1-f:.15f}, {i}, {t:.2f}')
             info(f'Loss: {f:.15f}, Fidelity: {1-f:.15f}, {i}, {t:.2f}')
     return f, g
@@ -47,17 +47,18 @@ def callback(xk):
 path = f'./data_322'  # path of folder
 dict_mat = file_dict(path)  # dict of mat files
 num = input('File name: num')  # input num of file index
-RDM_name = dict_mat[f'RDM_{num}']  # RDM mat file name
-model = re.search('model\d+', RDM_name).group(0)  # model number
+name = dict_mat[f'data_322_{num}']  # mat file name
+model = re.search('model\d+', name).group(0)  # model number
 layers = int(input('Number of layers: '))  # input number of layers
 
 log = f'./data_322/Logs/num{num}_{model}_L{layers}.log'
 basicConfig(filename=log, format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S', level=INFO)
 
-rdm3 = loadmat(f'{path}/RDM/{RDM_name}')['RDM_3']
-info(f'RDM3 Rank: {matrix_rank(rdm3)}')
-s = File(f'{path}/target_state/{dict_mat[f"target_state_{num}"]}', 'r')
-state = s['target_state_vec'][:].view('complex').conj()  # bra -> ket
+s = File(f'{path}/{name}', 'r')
+s_name = [x for x in s.keys() if 'state' in x]  # list of target_state_vec_?
+key = lambda x: [int(y) if y.isdigit() else y for y in re.split('(\d+)', x)]
+s_name = sorted(s_name, key=key)  # sort 1,10,11,...,2 into 1,2,...,10,11
+state = {i + 1: s[j][:].view('complex') for i, j in enumerate(s_name)}
 s.close()
 
 d = 3  # dimension of qudit state
@@ -80,15 +81,6 @@ info(f'Number of qubits: {nq}')
 info(f'Number of params: {p_num}')
 info(f'Number of gates: {g_num}')
 
-psi = su2_encoding(state, k + 1, is_csr=True)  # encode qutrit target state to qubit
-rho = psi.dot(psi.conj().T)  # rho & psi are both csr_matrix
-Ham = Hamiltonian(rho)  # set target state as Hamiltonian
-info(f'Hamiltonian Dimension: {rho.shape}')
-
-rho_rdm = reduced_density_matrix(state, d, position)
-info(f'rdm3 & rho norm L2:  {norm(rdm3 - rho_rdm, 2):.20f}')
-info(f'rdm3 & rho fidelity: {fidelity(rdm3, rho_rdm):.20f}')
-
 sim_list = set([i[0] for i in get_supported_simulator()])
 if 'mqvector_gpu' in sim_list and nq >= 12:
     sim = Simulator('mqvector_gpu', nq)
@@ -98,29 +90,39 @@ else:
     sim = Simulator('mqvector', nq)
     method = 'BFGS'  # TNC CG
     info(f'Simulator: mqvector, Method: {method}')
-sim_grad = sim.get_expectation_with_grad(Ham, ansatz)
 
-start = time.perf_counter()
-options = {'gtol': 1e-8, 'maxiter': 1e6}  # solver options
-p0 = np.random.uniform(-np.pi, np.pi, p_num)  # initial parameters
-res = minimize(fun, p0, args=(sim_grad, []), method=method, jac=True, callback=callback, options=options)
-info(res.message)
-info(f'Number of layers: {layers}')
-info(f'Optimal: {res.fun:.20f}, {res.fun}')
-print(f'Optimal: {res.fun:.20f}, {res.fun}')
+fidelity_list = []
+for vec in range(1, 19):
+    psi = su2_encoding(state[vec], k + 1, is_csr=True)  # encode qutrit state to qubit
+    rho = psi.dot(psi.conj().T)  # rho & psi are both csr_matrix
+    Ham = Hamiltonian(rho)  # set target state as Hamiltonian
+    info(f'State name: target_state_vec_{vec}')
 
-sim.reset()
-pr_res = dict(zip(p_name, res.x))  # optimal result parameters
-sim.apply_circuit(ansatz.apply_value(pr_res))  # apply result params to circuit
-psi_res = sim.get_qs()  # get result pure state
-psi_res = su2_decoding(psi_res, k + 1)  # decode qubit result state to qutrit
-rho_res_rdm = reduced_density_matrix(psi_res, d, position)
+    sim.reset()
+    sim_grad = sim.get_expectation_with_grad(Ham, ansatz)
 
-info(f'state & psi_res norm L2:  {norm(state - psi_res, 2):.20f}')
-info(f'state & psi_res fidelity: {fidelity(state, psi_res):.20f}')
-info(f'rdm3 & rho_res norm L2:  {norm(rdm3 - rho_res_rdm, 2):.20f}')
-info(f'rdm3 & rho_res fidelity: {fidelity(rdm3, rho_res_rdm):.20f}')
+    start = time.perf_counter()
+    options = {'gtol': 1e-8, 'maxiter': 1e4}  # solver options
+    p0 = np.random.uniform(-np.pi, np.pi, p_num)  # initial parameters
+    res = minimize(fun, p0, args=(sim_grad, []), method=method, jac=True, callback=callback, options=options)
+    info(res.message)
+    info(f'Number of layers: {layers}')
+    info(f'Optimal: {res.fun:.20f}, {res.fun}')
+    print(f'Optimal: {res.fun:.20f}, {res.fun}')
+    fidelity_list.append(1 - res.fun)
 
-total = time.perf_counter() - start
-print(f'Runtime: {total:.4f}s, {total/60:.4f}m, {total/3600:.4f}h, Iter: {res.nfev}')
-info(f'Runtime: {total:.4f}s, {total/60:.4f}m, {total/3600:.4f}h, Iter: {res.nfev}')
+    sim.reset()
+    pr_res = dict(zip(p_name, res.x))  # optimal result parameters
+    sim.apply_circuit(ansatz.apply_value(pr_res))  # apply result params to circuit
+    psi_res = sim.get_qs()  # get result pure state
+    psi_res = su2_decoding(psi_res, k + 1)  # decode qubit result state to qutrit
+    rho_res_rdm = reduced_density_matrix(psi_res, d, position)
+
+    info(f'state & psi_res norm L2:  {norm(state[vec] - psi_res, 2):.20f}')
+    info(f'state & psi_res fidelity: {fidelity(state[vec], psi_res):.20f}')
+
+    total = time.perf_counter() - start
+    print(f'Runtime: {total:.4f}s, {total/60:.4f}m, {total/3600:.4f}h, Iter: {res.nfev}')
+    info(f'Runtime: {total:.4f}s, {total/60:.4f}m, {total/3600:.4f}h, Iter: {res.nfev}')
+print(fidelity_list)
+info(fidelity_list)

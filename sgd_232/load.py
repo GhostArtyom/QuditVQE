4717d801e499d974bef6e6863020ba0ed28a02ec
GhostArtyom <ghostartyomns@gmail.com>
2024-03-19 15:37:26 +0800

    Upload: load.py

--- sgd_232/load.py
+++ sgd_232/load.py
@@ -1,6 +1,5 @@
 import re
 import os
-import numpy as np
 
 path = './sgd_232'
 files = [os.path.join(path, f) for f in sorted(os.listdir(path)) if 'log' in f]
@@ -12,20 +11,25 @@ for f in files:
     pattern = 'energy: (-?\d+\.\d+)'
     energy_list = [float(re.search(pattern, line).group(1)) for line in data if re.search(pattern, line) is not None]
     min_energy_list = min(energy_list) if len(energy_list) > 0 else ' No violation'
-    print(model, types, min_energy_list)
+    print(model, types, min_energy_list, len(data))
 
-files = [os.path.join(path, f) for f in sorted(os.listdir(path)) if 'txt' in f]
-for f in files:
-    energy_list = []
-    model = re.search('model\d+', f).group(0)
-    types = re.search('232_(.+)_model', f).group(1)
-    data = open(f).readlines()
-    pattern = '\[\d+, -\d+, \d+, (-?\d+\.\d+).+\],True'
-    energy_list = [float(re.search(pattern, line).group(1)) for line in data if re.search(pattern, line) is not None]
-    min_energy_list = min(energy_list) if len(energy_list) > 0 else ' No True Label'
-    print(model, types, min_energy_list, len(energy_list), len(data))
+# files = [os.path.join(path, f) for f in sorted(os.listdir(path)) if 'txt' in f]
+# for f in files:
+#     energy_list = []
+#     model = re.search('model\d+', f).group(0)
+#     types = re.search('232_(.+)_model', f).group(1)
+#     data = open(f).readlines()
+#     pattern = '\[\d+, -\d+, \d+, (-?\d+\.\d+).+\],True'
+#     energy_list = [float(re.search(pattern, line).group(1)) for line in data if re.search(pattern, line) is not None]
+#     min_energy_list = min(energy_list) if len(energy_list) > 0 else ' No True Label'
+#     print(model, types, min_energy_list, len(energy_list), len(data))
 
 '''
+20240319
+model1216 promising -9.017427013560935 718
+model1410 promising -4.031971471487899 446
+model1705 promising -5.041090250419363 1233
+model45 promising  No violation 0
 20240312
 model1216 all_types -9.018750842556233 180 60268
 model1410 all_types -4.039285783622273 266 50298

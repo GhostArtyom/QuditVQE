fdf972e4a86a04d13795d9abbd7f1271729df773
GhostArtyom <ghostartyomns@gmail.com>
2024-06-24 22:21:33 +0800

    Fix: colormap, view, scatter size & text position for PlotMajorana.m

--- PlotMajorana.m
+++ PlotMajorana.m
@@ -1,9 +1,8 @@
 clear; clf
 
-colormap parula
 [x, y, z] = sphere(50);
 surf(x, y, z, 'FaceAlpha', '0.5')
-hold on; axis equal
+hold on; axis equal; view(45, 30)
 
 color = get(groot, 'defaultAxesColorOrder');
 target = load('./data_322/target_coor_violation.mat');
@@ -16,7 +15,7 @@ for site = 1:n_qudits
     coor = prepared.(sprintf('num%d_D%d_vec40_site%d', num, D, site));
     for i = 1:size(coor, 1)
         x = coor(i, 1); y = coor(i, 2); z = coor(i, 3);
-        scatter3(x, y, z, 100, color(site, :), 'filled')
-        text(x, y, z + 0.1, num2str(site), 'FontSize', 12)
+        scatter3(x, y, z, 150, color(site, :), 'filled')
+        text(x, y, z + 0.08, num2str(site), 'FontSize', 12)
     end
 end

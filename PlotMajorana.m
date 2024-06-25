a5e309e1a2b5e13d1ebb5dedbb5792838bf78123
GhostArtyom <ghostartyomns@gmail.com>
2024-06-25 23:24:23 +0800

    Feature: MajoranaPoints(coor, site)

--- PlotMajorana.m
+++ PlotMajorana.m
@@ -4,15 +4,21 @@ clear; clf
 surf(x, y, z, 'FaceAlpha', '0.5')
 hold on; axis equal; view(45, 30)
 
-color = get(groot, 'defaultAxesColorOrder');
 target = load('./data_322/target_coor_violation.mat');
 prepared = load('./data_322/prepared_coor_violation.mat');
 
 n_qudits = 7;
-num = 1; D = 5;
+num = 1; D = 5; vec = 40;
 for site = 1:n_qudits
-    coor = target.(sprintf('num%d_D%d_vec40_site%d', num, D, site));
-    coor = prepared.(sprintf('num%d_D%d_vec40_site%d', num, D, site));
+    field = sprintf('num%d_D%d_vec%d_site%d', num, D, vec, site);
+    coor_target = target.(field);
+    coor_prepared = prepared.(field);
+    MajoranaPoints(coor_target, site)
+    MajoranaPoints(coor_prepared, site)
+end
+
+function MajoranaPoints(coor, site)
+    color = get(groot, 'defaultAxesColorOrder');
     for i = 1:size(coor, 1)
         x = coor(i, 1); y = coor(i, 2); z = coor(i, 3);
         scatter3(x, y, z, 150, color(site, :), 'filled')

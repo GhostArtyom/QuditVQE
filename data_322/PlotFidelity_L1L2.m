3ce3052f2420477bca9f066ee25b8e2336901fd3
GhostArtyom <ghostartyomns@gmail.com>
2025-04-29 19:17:35 +0800

    Upload: fidelity_322_num1~5_L1L2&L2.pdf

--- data_322/PlotFidelity_L1L2.m
+++ data_322/PlotFidelity_L1L2.m
@@ -1,53 +1,56 @@
 clear; clf; close all
 
 D = 5;
-font_size = 14;
+font_size = 16;
 line_width = 1.5;
 L_list = [-6 -6 -3 -4 -8];
 Q_list = [-6.32747 -6.33712 -3.20711 -4.14623 -8.12123];
 xmin_list = [0.02 0.02 0.01 0.008 0.008];
 xmax_list = [0.032 0.04 0.02 0.018 0.012];
+xlim_list = [-6.35 -5.97; -6.36 -5.97; -3.22 -2.982; -4.156 -3.987; -8.13 -7.989];
 
 for num = 1:5
     fig = figure(num);
-    xmin = []; xmax = [];
-    ymin = []; ymax = [];
     for layers = 1:2
         loadmat = load(sprintf('fidelity_energy_L%d.mat', layers));
         [xdata, ydata] = plotFidelity(loadmat, layers, num, D);
-        xmin = [xmin min(xdata)]; xmax = [xmax max(xdata)];
-        ymin = [ymin min(ydata)]; ymax = [ymax max(ydata)];
     end
-
     L_bound = L_list(num); Q_bound = Q_list(num);
     L_label = ['classical bound ${\cal{L}}$ = ', num2str(L_bound)];
-    Q_label = ['quantum limit ${\cal{Q}}$ = ', num2str(Q_bound)];
-    xline([L_bound Q_bound], '-.', {L_label, Q_label}, 'FontSize', font_size - 2, 'Interpreter', 'latex', ...
-    'LineWidth', line_width, 'Color', 'r', 'LabelHorizontalAlignment', 'left', 'LabelVerticalAlignment', 'mid');
-    xlabel('Ground state energy density', 'Interpreter', 'latex', 'rotation', 0, 'FontSize', font_size);
-    ylabel('NLF-per-site', 'Interpreter', 'latex', 'FontSize', font_size);
+    if num == 1 || num == 5
+        Q_label = ['quantum limit ${\cal{Q}}$ = ', num2str(Q_bound)];
+    else
+        Q_label = ['$\quad$quantum limit ${\cal{Q}}$ = ', num2str(Q_bound)];
+    end
+    xline([L_bound Q_bound], '-.', {L_label, Q_label}, 'FontSize', font_size-1, ...
+        'Interpreter', 'latex', 'LineWidth', line_width, 'Color', 'r', ...
+        'LabelHorizontalAlignment', 'left', 'LabelVerticalAlignment', 'mid');
     set(gca, 'XDir', 'reverse'); set(gca, 'YScale', 'log');
-    xmin = min(xmin); xmax = max(xmax);
-    ymin = min(ymin); ymax = max(ymax);
-    xlim = xmax - xmin;
-    set(gca, 'XLim', [xmin - xlim / 15, xmax + xlim / 10]);
-    set(gca, 'YLim', [10 ^ -9, 10 ^ -1]); yticks(10 .^ (-9:-1));
+    set(gca, 'XLim', xlim_list(num, :));
+    if num == 1
+        xticks(-6.3:0.05:-6);
+    elseif num == 3
+        xticks(-3.2:0.04:-3);
+    elseif num == 4
+        xticks(-4.15:0.03:-4);
+    end
+    set(gca, 'YLim', [10^-9, 10^-1]); yticks(10.^(-9:2:-1));
     set(gca, 'fontname', 'Times New Roman', 'FontSize', font_size);
     set(gcf, 'units', 'centimeters', 'Position', [5, 5, 18, 12]);
+    xlabel('Ground state energy density', 'Interpreter', 'latex', 'rotation', 0, 'FontSize', font_size+4);
+    ylabel('NLF-per-site', 'Interpreter', 'latex', 'FontSize', font_size+2);
     legend({'$D = 5, K = 1$', '$D = 5, K = 2$'}, 'Interpreter', 'latex', 'Position', [0.41 0.7 0.2 0.1]);
     print(fig, sprintf('./fig/fidelity_322_num%d_L1L2.pdf', num), '-r1000', '-dpdf');
 end
-
-close all
 
 function [xdata, ydata] = plotFidelity(loadmat, layers, num, D)
     n_qubits = 14; line_width = 1.5; marker_size = 15;
     color_list = ["#388E3C" "#4673BE"]; color = color_list(layers);
     data = loadmat.(sprintf('num%d_D%d', num, D));
     xdata = data.energy; ydata = data.fidelity;
-    ind = [5 4 4 5 4];
+    ind = [3 4 4 4 3];
     xdata = xdata(ind(num):end); ydata = ydata(ind(num):end);
-    if num == 3
+    if num == 3 && D == 5
         xdata(2) = []; ydata(2) = [];
     end
     ydata = -log(sqrt(ydata)) / n_qubits;
--- data_322/PlotFidelity_L2.m
+++ data_322/PlotFidelity_L2.m
@@ -1,51 +1,59 @@
 clear; clf; close all
 
 layers = 2;
-loadmat = load(sprintf('fidelity_energy_L%d.mat', layers));
-
 font_size = 14;
 line_width = 1.5;
 L_list = [-6 -6 -3 -4 -8];
 Q_list = [-6.32747 -6.33712 -3.20711 -4.14623 -8.12123];
 xmin_list = [0.02 0.02 0.01 0.008 0.008];
 xmax_list = [0.032 0.04 0.02 0.018 0.012];
+xlim_list = [-6.35 -5.97; -6.36 -5.97; -3.22 -2.982; -4.156 -3.987; -8.13 -7.989];
+ymax_list = [-3 -5 -3 -2 -3];
 
 for num = 1:5
     fig = figure(num);
-    xmin = []; xmax = [];
-    ymin = []; ymax = [];
     for D = 9:-1:5
+        loadmat = load(sprintf('fidelity_energy_L%d.mat', layers));
         [xdata, ydata] = plotFidelity(loadmat, num, D);
-        xmin = [xmin min(xdata)]; xmax = [xmax max(xdata)];
-        ymin = [ymin min(ydata)]; ymax = [ymax max(ydata)];
     end
     L_bound = L_list(num); Q_bound = Q_list(num);
     L_label = ['classical bound ${\cal{L}}$ = ', num2str(L_bound)];
-    Q_label = ['quantum limit ${\cal{Q}}$ = ', num2str(Q_bound)];
-    xline([L_bound Q_bound], '-.', {L_label, Q_label}, 'FontSize', font_size - 2, 'Interpreter', 'latex', ...
-    'LineWidth', line_width, 'Color', 'r', 'LabelHorizontalAlignment', 'left', 'LabelVerticalAlignment', 'top');
-    xlabel('Ground state energy density', 'Interpreter', 'latex', 'rotation', 0, 'FontSize', font_size);
-    ylabel('NLF-per-site', 'Interpreter', 'latex', 'FontSize', font_size);
+    Q_label = ['$\quad$quantum limit ${\cal{Q}}$ = ', num2str(Q_bound)];
+    xline([L_bound Q_bound], '-.', {L_label, Q_label}, 'FontSize', font_size-1, ...
+        'Interpreter', 'latex', 'LineWidth', line_width, 'Color', 'r', ...
+        'LabelHorizontalAlignment', 'left', 'LabelVerticalAlignment', 'mid');
     set(gca, 'XDir', 'reverse'); set(gca, 'YScale', 'log');
-    xmin = min(xmin); xmax = max(xmax);
-    ymin = min(ymin); ymax = max(ymax);
-    xlim = xmax - xmin;
-    set(gca, 'XLim', [xmin - xlim / 15, xmax + xlim / 10]);
-    set(gca, 'YLim', [10 ^ -9, 10 ^ -2]); yticks(10 .^ (-9:-2));
+    set(gca, 'XLim', xlim_list(num, :));
+    if num == 1
+        xticks(-6.3:0.05:-6);
+    elseif num == 3
+        xticks(-3.2:0.04:-3);
+    elseif num == 4
+        xticks(-4.15:0.03:-4);
+    end
+    ymax = ymax_list(num);
+    set(gca, 'YLim', [10^-9, 10^ymax]); yticks(10.^(-9:ymax));
     set(gca, 'fontname', 'Times New Roman', 'FontSize', font_size);
     set(gcf, 'units', 'centimeters', 'Position', [5, 5, 18, 12]);
+    xlabel('Ground state energy density', 'Interpreter', 'latex', 'rotation', 0, 'FontSize', font_size+4);
+    ylabel('NLF-per-site', 'Interpreter', 'latex', 'FontSize', font_size+2);
     legend({'$D = 9$', '$D = 8$', '$D = 7$', '$D = 6$', '$D = 5$'}, ...
         'Interpreter', 'latex', 'Location', 'north', 'Direction', 'reverse');
-    print(fig, sprintf('./fig/fig_num%d_L%d.pdf', num, layers), '-r1000', '-dpdf');
+    print(fig, sprintf('./fig/fidelity_322_num%d_L%d.pdf', num, layers), '-r1000', '-dpdf');
 end
 
 function [xdata, ydata] = plotFidelity(loadmat, num, D)
     n_qubits = 14; line_width = 1.5; marker_size = 15;
     color_dict = dictionary(5:9, ["#4474C4" "#BED8EF" "#C6E1B8" "#FEE69A" "#F8CDAD"]);
-    alpha = (D == 5) * 1 + (D ~= 5) * 0.5;
+    alpha = (D == 5) * 1 + (D ~= 5) * 0.8;
     color = [hex2rgb(color_dict(D)) alpha];
     data = loadmat.(sprintf('num%d_D%d', num, D));
     xdata = data.energy; ydata = data.fidelity;
+    ind = [3 4 4 4 3];
+    xdata = xdata(ind(num):end); ydata = ydata(ind(num):end);
+    if num == 3 && D == 5
+        xdata(2) = []; ydata(2) = [];
+    end
     ydata = -log(sqrt(ydata)) / n_qubits;
     fig = plot(xdata, ydata, '.-', 'LineWidth', line_width, 'MarkerSize', marker_size, 'Color', color);
     hold on
